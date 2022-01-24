5d178c52ff727288f02ecb98129187a2c7af87e1
dnflsclsrn1 <wnsgur765z@naver.com>
2022-01-24 22:54:58 +0900

    feat: 검색 서비스 홈화면추가

--- src/main/java/jpabook/japshop/repository/OrderRepository.java
+++ src/main/java/jpabook/japshop/repository/OrderRepository.java
@@ -2,10 +2,13 @@ package jpabook.japshop.repository;
 
 import jpabook.japshop.domain.Order;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,5 +24,40 @@ public class OrderRepository {
     public Order findOne(Long id){
         return em.find(Order.class, id);
     }
-    //public List<Order> findAll(OrderSerch orderSerch)
+
+    public List<Order> findAllByString(OrderSearch orderSearch){
+
+        return em.createQuery("select o from Order o join o.member m" +
+                "where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+        //동적쿼리일때는 다가져와 상태값 널이면 주문이든 취소든 가져와 동적쿼리를 해결해야 하는게 많많치가 않다.
+    }
+
+    public  List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList();
+}
 }
