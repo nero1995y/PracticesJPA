f326cf0cbe9b602a8213e421dcc1c80d4ded32dc
dnflsclsrn1 <wnsgur765z@naver.com>
2022-01-29 23:02:14 +0900

    feat: 상품등록

--- src/main/java/jpabook/japshop/controller/MemberController.java
+++ src/main/java/jpabook/japshop/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,4 +45,13 @@ public class MemberController {
         return "redirect:/";
     }
     //valid 쓰면 벨리데이션 가능
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        // DTO를 변환하여 화면에 필요한 것들을 뿌리기를 권장 드린다. 지금은 트레이드 오프땜에 ㄱㅊ API 를 만들때는 엔티티를 외부로 반환하지말자.
+        //API 스펙이 불완전해진다. 템플릿엔진은 덜 고려해도 되지만 제일깔끔한것들이다.
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
